3dd17e3f2ea5ee999d58847f705030916a8e001d
icellyl <jefersonharisson15@gmail.com>
2022-06-03 20:01:29 -0300

    Update Funcionario.java

--- src/main/java/java/com/Entidades/Funcionario.java
+++ src/main/java/java/com/Entidades/Funcionario.java
@@ -6,7 +6,7 @@ public class Funcionario {
     private String email;
     private String senha;
     private String funcao;
-//odelta
+//odeltal
     public String getNome() {
         return nome;
     } 
