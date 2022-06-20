150296c6af3ef7d7cbe224ecf33b18aeae073897
Melkizedek Franco Müller <melki.muller@gmail.com>
2022-06-20 20:04:13 -0300

    Melk: adiciona funcionário

--- src/main/java/com/persistencia/MVC/controller/FuncionarioController.java
+++ src/main/java/com/persistencia/MVC/controller/FuncionarioController.java
@@ -1,19 +1,24 @@
 package com.persistencia.MVC.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.persistencia.MVC.model.Funcionario;
 import com.persistencia.MVC.repository.FuncionarioRepository;
 
 @RestController
-@RequestMapping("/funcionarios")
+@RequestMapping("/funcionario")
 public class FuncionarioController {
     
-    
-
     @Autowired
-    private FuncionarioRepository funcionarioRepository;
+    private FuncionarioRepository FuncionarioRepository;
 
+    @PostMapping
+    public Funcionario adicionar(Funcionario funcionario) {
+        return FuncionarioRepository.save(funcionario);
+    }
+    
 
 }
