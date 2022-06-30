ecfd441c59ed6e025905f28953eb7e820e13cd02
Melkizedek Franco Müller <melki.muller@gmail.com>
2022-06-29 22:07:57 -0300

    Utiliza o Redirect View

--- src/main/java/com/persistencia/MVC/controller/FuncionarioController.java
+++ src/main/java/com/persistencia/MVC/controller/FuncionarioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,8 +23,9 @@ public class FuncionarioController {
     private FuncionarioRepository FuncionarioRepository;
 
     @PostMapping
-    public Funcionario adicionar(Funcionario funcionario) {
-        return FuncionarioRepository.save(funcionario);
+    public RedirectView adicionar(Funcionario funcionario) {
+         FuncionarioRepository.save(funcionario);
+         return new RedirectView("/back/funcionarios");
     }
 
     @GetMapping
