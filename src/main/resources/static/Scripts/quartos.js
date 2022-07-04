782e6212f12eb7bd8d5ca6104b81f33b92b45649
P4ul1t0s <paulomafra2004@gmail.com>
2022-07-04 19:40:23 -0300

    Update implementado

--- src/main/resources/static/Scripts/quartos.js
+++ src/main/resources/static/Scripts/quartos.js
@@ -7,12 +7,12 @@ $.ajax({
     success: function(arrayDeQuartos){
         for (let quarto of arrayDeQuartos) {
 
-            listaQuartos +=`<li class="objetos">
-                                ID do quarto: ${quarto.id}<br>
-                                Categoria: ${quarto.categoria}<br>
-                                Adultos (qtd): ${quarto.capacidadeAdultos}<br>
-                                Crianças (qtd): ${quarto.capacidadeCriancas}<br>
-                                Valor diária: R$${quarto.diaria}<br>
+            listaQuartos +=`<li id_=${quarto.id} class="objetos" onClick="editarQuarto(${quarto.id})">
+                                <div class="idReal">ID do quarto: <span>${quarto.id}</span></div>
+                                <div class="categoria">Categoria: <span>${quarto.categoria}</span></div>
+                                <div class="adultos">Adultos (qtd): <span>${quarto.capacidadeAdultos}</span></div>
+                                <div class="criancas">Crianças (qtd): <span>${quarto.capacidadeCriancas}</span></div>
+                                <div class="diaria">Valor diária: R$<span>${quarto.diaria}</span></div>
                                 Ocupação: ${quarto.ocupado}<br>
                                 <a href='/back/quartos/deletar/${quarto.id}'>Excluir</a>
                             </li>`
@@ -26,3 +26,12 @@ $.ajax({
         
     }
 })
+
+function editarQuarto(id_quarto){
+    $("#id-quarto-selecionado").html($(`li[id_=${id_quarto}].objetos`).find(".idReal span").text())
+    $("#idHidden").val($(`li[id_=${id_quarto}].objetos`).find(".idReal span").text())
+    $("#categoria_quarto").val($(`li[id_=${id_quarto}].objetos`).find(".categoria span").text())
+    $("#capacidadeA_quarto").val($(`li[id_=${id_quarto}].objetos`).find(".adultos span").text())
+    $("#capacidadeC_quarto").val($(`li[id_=${id_quarto}].objetos`).find(".criancas span").text())
+    $("#diaria_quarto").val($(`li[id_=${id_quarto}].objetos`).find(".diaria span").text())
+}
